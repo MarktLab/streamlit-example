c1b396799f38fe22753e7ff40085ee4a3d36f3e4
MarktLab <162396841+MarktLab@users.noreply.github.com>
2024-03-07 04:00:28 +0000

    ll

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,21 +14,29 @@ if "openai_model" not in st.session_state:
 if "messages" not in st.session_state:
     st.session_state.messages = []
 
-# Function to append messages and get response
-def append_and_get_response(new_message):
-    st.session_state.messages.append({"role": "user", "content": new_message})
-    
-    stream = client.chat.completions.create(
-        model=st.session_state["openai_model"],
-        messages=[
-            {"role": m["role"], "content": m["content"]}
-            for m in st.session_state.messages
-        ],
-        stream=True,
-    )
-    response = st.write_stream(stream)
-    st.session_state.messages.append({"role": "assistant", "content": response})
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
 
+# Function to append messages and get response
+def append_and_get_response(prompt):
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    with st.chat_message("user"):
+        st.markdown(prompt)
+
+    with st.chat_message("assistant"):
+        stream = client.chat.completions.create(
+            model=st.session_state["openai_model"],
+            messages=[
+                {"role": m["role"], "content": m["content"]}
+                for m in st.session_state.messages
+            ],
+            stream=True,
+        )
+        response = st.write_stream(stream)
+    st.session_state.messages.append({"role": "assistant", "content": response})
+    
+    
 # Form for initial user input
 with st.form("user_info"):
     nonprofitName = st.text_input("Nonprofit Name")
